5859d94ea417384dae1671559f0111502d0c80c8
TechnoStudy <ismettechno@gmail.com>
2025-02-27 17:33:31 +0000

    yeni Login Testi yapıldı

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -4,12 +4,11 @@ public class LoginTesti {
     public static void main(String[] args) {
 
         // Bu bölüm Login testi için, yeni şifre modeline göre yazıldı
-        System.out.println("Yeni Login Testi ");
-
-        // çok satır yazdım ve kodları bitirdim
-        System.out.println("Login Testi 1");
+        System.out.println("Yeni Login Testi 1");
 
         System.out.println("Sepet testi yazıldı bitirildi,3 günlük kod yazıldı");
+
+        System.out.println("Yeni Login Testi yapıldı, 7 gün uğraştım");
 
     }
 }
