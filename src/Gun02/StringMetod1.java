863c3c9c0d1b2e7e0fe225b0a26465ed04d60a44
TechnoStudy <ismettechno@gmail.com>
2025-02-27 18:41:03 +0000

    StringMetod1

---
+++ src/Gun02/StringMetod1.java
@@ -0,0 +1,11 @@
+package Gun02;
+
+public class StringMetod1 {
+    public static void main(String[] args) {
+
+        String kelime="ismet";
+
+        System.out.println(kelime.length());
+
+    }
+}
